cb2259ee7fec516dff50f1a2cf23cbda18e415d5
SashaResh <117270097+SashaResh@users.noreply.github.com>
2022-11-30 13:36:09 +0500

    Add files via upload

---
+++ Algorithms/2-15/2.15.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    int n, s, k;
+    bool flag = false;
+
+    cin >> n >> s;
+
+    while (n > 0 && !flag)
+    {
+        k = n % 10;
+        if (k == s)
+        {
+            flag = true;
+            cout << "Цифра найдена" << endl;
+        }
+        else
+        {
+            n /= 10;
+        }
+    }
+
+    if (!flag)
+    {
+        cout << "Цифра не найдена" << endl;
+    }
+
+    return 0;
+}
