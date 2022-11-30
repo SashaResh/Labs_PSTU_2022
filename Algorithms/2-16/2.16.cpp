993b790ffb891f3845bc75c4cdb6d7b100283f94
SashaResh <117270097+SashaResh@users.noreply.github.com>
2022-11-30 13:36:33 +0500

    Add files via upload

---
+++ Algorithms/2-16/2.16.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+    int n;
+    int sum = 0;
+
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            sum += i;
+        }
+        else
+        {
+            sum -= i;
+        }
+    }
+    cout << sum << endl;
+
+	return 0;
+}
